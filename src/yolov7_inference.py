380a99ec802cc91348d4b19498cfad09714db050
Tamagusko <tamagusko@gmail.com>
2023-07-16 14:43:16 +0100

    fix: yolov7 inference

--- src/yolov7_inference.py
+++ src/yolov7_inference.py
@@ -12,12 +12,12 @@ def parse_yaml(yaml_file):
 
 def detect_yolov7(params):
     for version in params['versions']:
-        command = f"python {params['yolo_path']}/detect.py --weights {os.getcwd()}/models/yolov7{version}_best.pt --img {params['img_size']} --source {params['data_path']} --project {params['project_name']} --name {params['run_name']}_{version} --save-txt"
+        command = f"python {params['yolo_path']}/test.py --weights {os.getcwd()}/models/{version}_best_pothole.pt --img {params['img_size']} --data {os.path.join(params['data_path'], 'data_local.yaml')} --project {params['project_name']} --name {params['run_name']}_{version} --save-txt"
         subprocess.run(command, shell=True)
 
 
 def main():
-    params = parse_yaml('param_detect_YOLOv7.yaml')
+    params = parse_yaml('src/param_YOLOv7.yaml')
     detect_yolov7(params)
 
 
