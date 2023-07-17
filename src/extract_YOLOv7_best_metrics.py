819d7088d99e39a04e4c8c1fa52d68ac2302b86f
Tamagusko <tamagusko@gmail.com>
2023-07-17 23:51:00 +0100

    feat: yolov7 best metrics template

---
+++ src/extract_YOLOv7_best_metrics.py
@@ -0,0 +1,44 @@
+# NEED TO BE ADJUSTED FOR THE results.csv columns
+
+import numpy as np
+import pandas as pd
+
+def extract_best_metrics(csv_file_path):
+    """Extract best metrics from csv.
+
+    Args:
+        csv_file_path(str): Path to the csv file.
+
+    Returns:
+        str: A string representation of best metrics.
+    """
+    # Load the CSV data into a DataFrame
+    data = pd.read_csv(csv_file_path)
+    
+    # Compute the fitness value based on the given weights
+    fitness_values = 0.1 * data['metrics/mAP50(B)'] + 0.9 * data['metrics/mAP50-95(B)']
+
+    # Get the index of the best fitness value
+    best_index = np.argmax(fitness_values)
+
+    # Get the row with the best metrics
+    best_metrics = data.loc[best_index]
+
+    result_str = f"best epoch: {best_metrics['epoch']}, mAP_0.5: {best_metrics['metrics/mAP50(B)']}, mAP_0.5:0.95: {best_metrics['metrics/mAP50-95(B)']}"
+    
+    return result_str
+
+def main():
+    # Model versions
+    versions = ['YOLOv7', 'YOLOv7x']
+
+    for version in versions:
+        # Form the CSV path for each model
+        csv_path = f'/home/t1/repos/pothole-detection/{version}/train_comparison/results.csv'
+        
+        best_metrics = extract_best_metrics(csv_path)
+        print(f"model: {version}, {best_metrics}")
+
+if __name__ == "__main__":
+    main()
+
